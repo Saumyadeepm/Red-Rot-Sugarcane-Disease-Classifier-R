be6d5d7a616eac87ea43d187dc09bfaa5965ad1b
Saumyadeep Mitra <saumyadeep.1221@gmail.com>
2023-10-31 16:45:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.set_page_config(
     menu_items={
         'Get Help': 'https://www.saumyadeepmitra.live',
         'Report a bug': 'https://www.saumyadeepmitra.live/contact',
-        'About': "Copyright © 2023 Saumyadeep Mitra. All rights reserved."
+        'About': "# Copyright © 2023 Saumyadeep Mitra. All rights reserved."
     }
 )
 
